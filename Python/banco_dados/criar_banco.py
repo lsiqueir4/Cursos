c2f2d27299d713d61f6b0d7857b3f10ba263ecd2
Leandro Siqueira <link_le@hotmail.com>
2021-12-13 17:48:36 -0300

    Python_bd_1

---
+++ Python/banco_dados/criar_banco.py
@@ -0,0 +1,17 @@
+try:
+    from mysql.connector import connect
+except:
+    print ('BIBLIOTECA NAO INSTALADA')
+else:
+    print ('BIBLIOTECA INSTALADA E OK')
+
+
+conexao = connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    passwd = '1234'
+)
+
+cursor = conexao.cursor()
+cursor.execute('CREATE DATABASE agenda') #.execute executa um comando em sql
